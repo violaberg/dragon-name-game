d1f2953cc8fe84202a9f5d6e4501360f21c1c955
Viola Bergere <viola.bergere@gmail.com>
2023-10-20 17:55:04 +0000

    Remove whitespaces

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ def main_logo():
     Print the name of the game
     """
     logo = [
-        " ____",                                 
+        " ____",
         "|  _ \ _ __ __ _  __ _  ___  _ __  ___",
         "| | | | '__/ _` |/ _` |/ _ \| '_ \/ __|",
         "| |_| | | | (_| | (_| | (_) | | | \_ \_",
         "|____/|_|  \__,_|\__, |\___/|_| |_|___/",
-        "                 |___/",               
+        "                 |___/",
     ]
     for line in logo:
         print(line)
@@ -30,30 +30,30 @@ def intro():
     ]
     for line in game_intro:
         print(line)
-        
-       
+
+
 def continue_to_game(username, dragon_list):
     """
     Verifies user wants to play the game by entering y for yes or n for no
     """
     choice = input("Would you like to play (yes/no)?: ").lower()
-    
+
     if choice in ["yes", "y"]:
         start_game(username, dragon_list)
     elif choice in ["no", "n"]:
         exit_game()
     else:
         print("Please enter yes or no")
-        
-        
+
+
 def exit_game():
     """
     Exits game gracefully and thanks user for visiting.
     """
     print("Thank you for visiting 'Dragons' game. Goodbye!")
     exit()
-        
-        
+
+
 def enter_username():
     """
     Asks user to create a username consisting of 4 - 15 characters.
@@ -61,16 +61,16 @@ def enter_username():
     while True:
         print()
         username = input("Please enter username:\n")
-        
+
         if " " in username:
             print("Username can't contain empty spaces!")
-            
+
         if len(username) < 4:
             print("Sorry, username has to contain at least 4 characters!")
-            
+
         if len(username) > 15:
             print("Sorry, username can't be longer than 15 characters!")
-            
+
         else:
             return username
 
@@ -102,30 +102,30 @@ def start_game(username, dragon_list):
     name_to_guess = random_name(dragon_list)
     guessed_letters = []
     attempts = 8
-    
+
     print(f"Welcome, {username}!")
     print(f"You have {attempts} attempts to guess the name.")
-    
+
     while attempts > 0:
         print(f"Name to guess: {display_name(name_to_guess, guessed_letters)}")
         guess = input("Guess a letter: ").lower()
-        
+
         if len(guess) != 1 or not guess.isalpha():
             print("Please enter one letter!")
             continue
-        
+
         if guess in guessed_letters:
             print("Oh, sorry, you have already guessed that letter!")
             continue
-        
+
         guessed_letters.append(guess)
-        
+
         if guess in name_to_guess:
             print("Great guess! You are one step closer to revealing dragon!")
         else:
             print("Aw, incorrect! Please try again!")
             attempts -= 1
-            
+
         if "_" not in display_name(name_to_guess, guessed_letters):
             print("Congrats! You are a true dragon master!")
             print(f"You guessed {name_to_guess} dragon!")
@@ -134,7 +134,7 @@ def start_game(username, dragon_list):
         if "_" in display_name(name_to_guess, guessed_letters):
             print("Aw, sorry, you are out of attempts! Dragon got you!")
             print(f"Name to guess was: {name_to_guess}")
-            
+
 
 def main():
     """
